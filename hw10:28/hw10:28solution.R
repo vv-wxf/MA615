8beb32e84438dee4c50b3be59af47a4bce3c206d
chenn24 <chenn24@uw.edu>
2019-10-30 18:54:00 -0400

    10/28 hw final edition

--- hw10:28/hw10:28solution.R
+++ hw10:28/hw10:28solution.R
@@ -1,32 +1,33 @@
-# library(tidyverse)
-# library(RSQLite)
-# library(DBI)
-# library(readxl)
-# con<-dbConnect(SQLite(),"chinook.db")
-# 
-# dbGetQuery(con,"select FirstName,LastName from Customers where Country = 'Brazil';")
-# 
-# dbGetQuery(con,"select FirstName,LastName from customers where Country = 'Brazil';")
-
-
 # hw
 
 library(tidyverse)
 library(readxl)
+library(RSQLite)
+library(DBI)
 
 donars_des <- read_excel("Top MA Donors 2016-2020.xlsx",sheet = 1)
 contrib_all <- read_excel("Top MA Donors 2016-2020.xlsx",sheet = 2)
 JFC <- read_excel("Top MA Donors 2016-2020.xlsx",sheet = 3)
 
+
+# data cleaning
+## Turn full name into first name and delete all unnecessary part in the name
+## delete the '.' at the end of the Fecoccemp
+DF <- contrib_all %>% separate(contrib, c("last", "first"), ",") %>%
+  separate(first, c("redundancy", "firstname"), " ") %>%
+  dplyr::select(-redundancy,-last)
+DF$Fecoccemp <- as.factor(gsub("\\.", "", DF$Fecoccemp))
+
+
 # contribid --A unique identifier for individual donors. 
-contributors <- select(contrib_all,
-                       contribid,fam,contrib,City,State,Zip,Fecoccemp,orgname,lastname)%>%distinct()
+contributors <- select(DF,
+                       contribid,fam,firstname,lastname,City,State,Zip,Fecoccemp,orgname)%>%distinct()
 
-orgs <- select(contrib_all,orgname,ultorg)%>%distinct()
+orgs <- select(DF,orgname,ultorg)%>%distinct()
 
-contribution <- select(contrib_all,
+contribution <- select(DF,
                        fectransid,contribid,date,amount,recipid,cycle,type,cmteid)%>%distinct()
-recipients <- select(contrib_all,
+recipients <- select(DF,
                      recipid,recipient,party,recipcode,cmteid)%>%distinct()
 
 mydb = dbConnect(SQLite(),"Political Contribution_vivian.sqlite")
